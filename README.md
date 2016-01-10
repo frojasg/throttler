5159c5cb3eb86dc8c7d13019106b73a41eb7416d
Francisco Rojas <frojas@alumnos.utalca.cl>
2016-01-09 19:26:15 -0800

    Initial implemenatation timer-based throttler
    It use the Generic Finite State Machine behavior from OTP/erlang gen_fsm

--- README.md
+++ README.md
@@ -1,38 +1,38 @@
 # Throttler
 
 ## What's this about?
-Let's say you application needs to make calls to an external service which has a restriction usage. You may only make X calls in Y seconds. With a throtler, you can ensure that calls you make do not cross the threshold rate.
+Let's say you application needs to make calls to an external service which has a restriction usage. You may only make X calls in Y seconds. With a throttler, you can ensure that calls you make do not cross the threshold rate.
 
 # Example
 
 ```elixir
 # A simple actor that prints whatever it receives
 defmodule Printer do
-  use Genserver
-
-  def print(server, x) do
-    GenServer.cast(server, {print, x})
-  end
+  use GenServer
 
   def start(opts \\ []) do
     GenServer.start(__MODULE__, :ok, opts)
   end
 
-  def handle_cast({:print, x}, _state) do
+  def handle_info({:print, x}, _state) do
     IO.puts "#{inspect x}"
-    {:noreply, _sate}
+    {:noreply, _state}
   end
 end
 
 
-printer = Printer.start
-throttler = TimerBasedThrottler.start(messages: 3, per: 3000, target: printer)
+{:ok, thr} = TimerBasedThrottler.start_link(messages: 3, period: 3*1*1000)
+{:ok, printer} = Printer.start
 
-Printer.print(throttler, 1)
-Printer.print(throttler, 2)
-Printer.print(throttler, 3)
+#set target
+TimerBasedThrottler.set_target(thr, printer)
 
+#msg
+TimerBasedThrottler.enqueue(thr, {:print, "Hello World"})
+TimerBasedThrottler.enqueue(thr, {:print, "Hello World"})
+TimerBasedThrottler.enqueue(thr, {:print, "Hello World"})
 
-Printer.print(throttler, 4)
-Printer.print(throttler, 5)
+TimerBasedThrottler.enqueue(thr, {:print, "Hello World"})
+TimerBasedThrottler.enqueue(thr, {:print, "Hello World"})
+
 ```
---
+++ lib/timer_based_throttler.ex
@@ -0,0 +1,105 @@
+defmodule TimerBasedThrottler do
+  @behaviour :gen_fsm
+  require Logger
+
+  @period 1*1000
+  @messages 10
+
+  defstruct [
+    target: nil,
+    messages_left_in_period: 0,
+    messages_per_period: 0,
+    period: 1000, #1 second
+    queue: [],
+    timer: nil
+  ]
+
+  #public
+  def start_link(args) do
+    :gen_fsm.start_link(__MODULE__, args, [])
+  end
+
+  def set_target(throttler, target) do
+    :gen_fsm.send_event(throttler, {:set_target, target})
+  end
+
+  def enqueue(throttler, msg) do
+    :gen_fsm.send_event(throttler, {:queue, msg})
+  end
+
+  #state machine
+  def idle({:set_target, target}, state = %TimerBasedThrottler{queue: []}) do
+    {:next_state, :idle, %{state | target: target}}
+  end
+
+  def idle({:set_target, target}, state) do
+    state = %{state | target: target} |> deliver_messages |> timer
+    {:next_state, :active, state}
+  end
+
+  def idle({:queue, msg}, state = %TimerBasedThrottler{target: nil, queue: q}) do
+    {:next_state, :idle, %{state | queue: ([msg | q] |> Enum.reverse)}}
+  end
+
+  def idle({:queue, msg}, state = %TimerBasedThrottler{target: target, queue: []}) when is_nil(target) == false do
+    state = %{state | queue: [msg]} |> deliver_messages |> timer
+    {:next_state, :active, state}
+  end
+
+  def idle(:tick, state) do
+    {:next_state, :active, %{state | timer: nil}}
+  end
+
+  def idle(_msg, state) do
+    {:next_state, :idle, state}
+  end
+
+  def active({:queue, msg}, state = %TimerBasedThrottler{queue: q}) do
+    state = %{state | queue: ([msg | q] |> Enum.reverse)} |> deliver_messages
+    {:next_state, :active, state}
+  end
+
+  def active(:tick, state = %TimerBasedThrottler{queue: []}) do
+    {:next_state, :idle, %{state | timer: nil, messages_left_in_period: state.messages_per_period}}
+  end
+
+  def active(:tick, state) do
+    state = %{state | messages_left_in_period: state.messages_per_period} |> deliver_messages |> timer
+    {:next_state, :active, state}
+  end
+
+  def deliver_messages(state) do
+    message_to_send = min(length(state.queue), state.messages_left_in_period)
+    state.queue |> Enum.take(message_to_send) |> Enum.each fn(message) -> send state.target, message end
+    %{state | queue: Enum.drop(state.queue, message_to_send), messages_left_in_period: (state.messages_left_in_period - message_to_send)}
+  end
+
+  def timer(state) do
+    %{state | timer: Process.send_after(self(), :tick, state.period)}
+  end
+
+  # OTP
+  def init(args) do
+    state = %TimerBasedThrottler{
+      messages_left_in_period: Keyword.get(args, :messages, @messages),
+      messages_per_period:  Keyword.get(args, :messages, @messages),
+      period:  Keyword.get(args, :period, @period),
+      timer: nil
+    }
+    {:ok, :idle, state}
+  end
+
+  def handle_info(:tick, state_name, state) do
+    # Logger.info "time! state_name: #{inspect state_name} state: #{inspect state}"
+    :gen_fsm.send_event(self, :tick)
+    {:next_state, state_name, state}
+  end
+
+  def terminate(reason, state_name, state) do
+    Logger.info "#{inspect state_name} reason: #{inspect reason} status: #{inspect state}"
+  end
+
+  def format_status(_reason, [ _pdict, state ]) do
+    [data: [{'State', "My current state is '#{inspect state}', and I'm happy"}]]
+  end
+end
