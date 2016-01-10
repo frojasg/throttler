4ac7fab1d3e8371d0c0b813777cfceecef10b3c3
Francisco Rojas <frojas@alumnos.utalca.cl>
2016-01-09 20:30:36 -0800

    Add Unit to test TimerBasedThrottler

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,15 @@
 ExUnit.start()
+
+defmodule Echo do
+  use GenServer
+  require Logger
+
+  def start(subject) do
+    GenServer.start(__MODULE__, subject)
+  end
+
+  def handle_info({:echo, message}, subject) do
+    send subject, message
+    {:noreply, subject}
+  end
+end
---
+++ test/timer_based_throttler_test.exs
@@ -0,0 +1,35 @@
+defmodule TimerBasedThrottlerTest do
+  use ExUnit.Case
+
+  test "keep messages until target is set" do
+    {:ok, thr} = TimerBasedThrottler.start_link(messages: 3, period: 3*1000)
+    {:ok, echo} = Echo.start(self)
+    TimerBasedThrottler.enqueue(thr, {:echo, :one})
+    TimerBasedThrottler.enqueue(thr, {:echo, :two})
+    TimerBasedThrottler.enqueue(thr, {:echo, :three})
+    refute_receive _
+    TimerBasedThrottler.set_target(thr, echo)
+    assert_receive :one
+    assert_receive :two
+    assert_receive :three
+  end
+
+  test "respect rate 3 messages per second" do
+    {:ok, thr} = TimerBasedThrottler.start_link(messages: 3, period: 3*1000)
+    {:ok, echo} = Echo.start(self)
+    TimerBasedThrottler.set_target(thr, echo)
+    TimerBasedThrottler.enqueue(thr, {:echo, :one})
+    TimerBasedThrottler.enqueue(thr, {:echo, :two})
+    TimerBasedThrottler.enqueue(thr, {:echo, :three})
+    TimerBasedThrottler.enqueue(thr, {:echo, :four})
+    TimerBasedThrottler.enqueue(thr, {:echo, :five})
+    assert_receive :one
+    assert_receive :two
+    assert_receive :three
+
+    refute_receive _, 3*1000
+    assert_receive :four
+    assert_receive :five
+  end
+
+end
